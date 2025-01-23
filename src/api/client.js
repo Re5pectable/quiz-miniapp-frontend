66c847e36c5cb05fe9c7dd4cd212a70abe1f7dc8
Басистов Глеб <basistovga@cash-u.com>
2025-01-23 06:39:56 +0300

    fix

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "https://quiz.kley.media",
+  baseURL: "https://quiz.kley.media/api",
   withCredentials: true,
 });

21b705aef9e9e6ba7824a1882df5d658a4c08ef7
Басистов Глеб <basistovga@cash-u.com>
2025-02-13 18:02:42 +0300

    fix

--- src/components/admin/_Wrapper.vue
+++ src/components/admin/_Wrapper.vue
@@ -9,21 +9,19 @@ export default {
 }
 </script>
 <style>
-body{
-    background-color: #F6F6F6;
-    color: #000000 !important;
-}
 .admin-page{
     padding: 0 20px;
     margin-top: 40px;
+    background-color: #F6F6F6;
+    color: #000000 !important;
 }
 
-h1,
-h2,
-h3,
-h4,
-p,
-input{
+.admin-page > * > h1,
+.admin-page > * > h2,
+.admin-page > * > h3,
+.admin-page > * > h4,
+.admin-page > * > p,
+.admin-page > * > input{
     color: #000000 !important;
 }
 </style>

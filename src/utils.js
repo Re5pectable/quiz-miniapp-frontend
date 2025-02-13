60ea0f8a20faa0640443f4ea3386d7108f1660fd
Басистов Глеб <basistovga@cash-u.com>
2025-02-13 15:26:16 +0300

    fix

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
-export default async function sleep(seconds) {
+export async function sleep(seconds) {
     await new Promise(r => setTimeout(r, seconds * 1000));
 }
 
-export default function toCamelCase(snakeCase) {
+export function toCamelCase(snakeCase) {
     return snakeCase.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
 }
 
-export default function toSnakeCase(camelCase) {
+export function toSnakeCase(camelCase) {
     return camelCase.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
 }
 
